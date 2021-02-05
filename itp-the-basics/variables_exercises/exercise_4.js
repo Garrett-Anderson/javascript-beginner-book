15f0128d7874cae9c509894a3c65396fc8f9cfb0
Garrett-Anderson <77418386+Garrett-Anderson@users.noreply.github.com>
2021-02-05 13:05:26 -0600

    Variables_Exercises

---
+++ itp-the-basics/variables_exercises/exercise_4.js
@@ -0,0 +1,10 @@
+let name = 'Victor';
+console.log('Good Morning, ' + name);
+console.log('Good Afternoon, ' + name);
+console.log('Good Evening, ' + name);
+
+name = 'Joe';
+console.log('Good Morning, ' + name);
+console.log('Good Afternoon, ' + name);
+console.log('Good Evening, ' + name);
+
