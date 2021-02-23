c8eadfaf2d4b3f39fce4c6bf649d905bda0ca0bd
Garrett-Anderson <77418386+Garrett-Anderson@users.noreply.github.com>
2021-02-23 14:31:44 -0600

    more stuff

---
+++ itp-the-basics/more-stuff/regex.js
@@ -0,0 +1,19 @@
+if (/b/.test('bobcat')) {
+  console.log("Yes, it contains the letter 'b'");
+} else {
+  console.log("No, it doesn't contain the letter 'b'");
+}
+
+function has_a_or_e(string) {
+  let results = string.match(/[ae]/g);
+  if (results) {
+    console.log(`We have a match! ${results}`);
+  } else {
+    console.log('No match here.');
+  }
+}
+
+has_a_or_e("basketball")
+has_a_or_e("football");
+has_a_or_e("hockey");
+has_a_or_e("golf");
---
+++ itp-the-basics/more-stuff/variables-as-pointers.js
@@ -0,0 +1,6 @@
+let count = 1;
+count = 2;
+
+let obj = { a: 1 };
+obj = { b: 2 };
+obj.c = 3;
